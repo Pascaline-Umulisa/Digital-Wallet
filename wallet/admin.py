d9e506525ccb338da5be1dff233800cb5c7bf497
Pascaline-Umulisa <kayiteteumulisa@gmail.com>
2022-08-02 11:33:06 +0300

    created instances

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 # Register your models here.
-from .models import Account, Card, Customer, Loan, Notification, Receipt, Reward, ThirdParty, Transaction, Wallet
+from .models import Account, Card, Customer,Currency, Loan, Notification, Receipt, Reward, ThirdParty, Transaction, Wallet
 admin. site.register(Customer)
 admin.site.register(Account)
 admin.site.register(Wallet)
 admin.site.register(Transaction)
+admin.site.register(Currency)
 admin.site.register(Card)
 admin.site.register(ThirdParty)
 admin.site.register(Notification)
